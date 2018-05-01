0b89a5cdfadf383e8e0b0f93d70e06d080749f26
olan <olantoj@hotmail.com>
2018-05-01 12:24:07 +0700

    centrality

---
+++ plot-with-centrality.py
@@ -0,0 +1,64 @@
+import glob
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+mahidolU_follower = []
+in_degree = {}
+out_degree = {}
+G = nx.DiGraph()
+
+count = 0
+
+with  open("data/mahidolU.txt") as f:
+    for line in f:
+        mahidolU_follower.append(line.rstrip('\n'))
+
+for id in mahidolU_follower:
+    in_degree[id] = 0
+    out_degree[id] = 0
+
+for file in glob.glob('data/clean_follower/*'):
+    current_node = file.strip('.txt').split('\\')[1]
+    if current_node not in G:
+        G.add_node(current_node)
+    try:
+        with open(file) as f:
+
+            for line in f:
+                line = line.rstrip('\n')
+                out_degree[current_node]+=1
+                in_degree[line]+=1
+                try:
+                    G.add_edge(line,current_node)
+                    print("add the edge")
+                    count+=1
+
+                except:
+                    print("add  node mai dai")
+
+    except:
+        print("read file mai dai")
+solo = 0
+for id in in_degree:
+    if in_degree[id] == 0 and out_degree[id] == 1:
+        print(id)
+        solo+=1
+print(solo)
+print(count)
+x = nx.in_degree_centrality(G);
+ #for key, value in sorted(x.items(), key=lambda item: (item[1], item[0])):
+    #print (key, ":", value)
+
+y = nx.out_degree_centrality(G);
+#for key, value in sorted(y.items(), key=lambda item: (item[1], item[0])):
+    #print (key, ":", value)
+
+z = nx.degree_centrality(G);
+#for key, value in sorted(z.items(), key=lambda item: (item[1], item[0])):
+    #print (key, ":", value)
+
+#nx.draw(G,node_size = 20,width = 0.5, edge_color = "grey")
+#plt.show()
+#plt.savefig("Graph.png", format="PNG")
